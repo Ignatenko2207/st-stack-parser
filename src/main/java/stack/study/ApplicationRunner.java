3d8676a4ac094e6576b5fb762bfa7cc5ab1ca517
Alex <ignatenko2207@gmail.com>
2019-09-07 15:35:45 +0300

    Initial commit

---
+++ src/main/java/stack/study/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package stack.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class ApplicationRunner
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/stack/study/ApplicationRunnerTest.java
@@ -0,0 +1,21 @@
+package stack.study;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple ApplicationRunner.
+ */
+public class ApplicationRunnerTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
